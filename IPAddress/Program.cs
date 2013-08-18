9728e09c6bcf0e0392ed18dbc3eb93682101cf6b
Karan Sharma <javakaran@live.com>
2013-08-19 02:18:52 +0530

    ip address of computer using C#

---
+++ IPAddress/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace MyIPAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                    Console.WriteLine(localIP);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
